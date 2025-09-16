0993e4b842a2de04e8231b7dc4cf5a8e3f03d321
Maria Tjelta <maria.tjelta@gmail.com>
2025-09-16 10:46:04 +0200

    added file and code

---
+++ Build-a-Time-Calculator-Project.py
@@ -0,0 +1,70 @@
+def add_time(start, duration, day=None):
+    # split hour, minutes and AM/PM
+    start_hour = int(start.split(':')[0])
+    start_min = int(start.split(':')[1].split(' ')[0])
+    start_pmam = start[-2:]
+
+    total_min = start_hour * 60 + start_min
+    if start_pmam == 'AM' and start_hour == 12:
+        total_min = 0 + start_min
+    elif start_pmam == 'PM' and start_hour != 12:
+        total_min = (start_hour + 12) * 60 + start_min
+
+    # split duration
+    duration_hour = int(duration.split(':')[0])
+    duration_min = int(duration.split(':')[1])
+    duration_total_min = duration_hour * 60 + duration_min
+
+    # add start and duration
+    end_total_min = total_min + duration_total_min
+
+    # Find total whole days and end minutes
+    days_later = end_total_min // 1440
+    end_min_in_day = end_total_min % 1440
+    
+    # convert back to hours and minutes
+    end_hour = end_min_in_day // 60
+    end_min = end_min_in_day % 60
+
+    # convert from 24 hours to 12 hours
+    if end_hour == 0:
+        final_hour = 12
+        period = "AM"
+    elif 1 <= end_hour <= 11:
+        final_hour = end_hour
+        period = "AM"
+    elif end_hour == 12:
+        final_hour = 12
+        period = "PM"
+    elif 13 <= end_hour <= 23:
+        final_hour = end_hour - 12
+        period = "PM" 
+        
+    if end_min < 10:
+        formatted_min = "0" + str(end_min)
+    else:
+        formatted_min = str(end_min)
+
+    if days_later == 0:
+        day_string = ""
+    elif days_later == 1:
+        day_string = " (next day)"
+    elif days_later > 1:
+        day_string = f" ({days_later} days later)"
+    
+    weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    if day is not None:
+        day_input = day.capitalize()
+        day_index = weekdays.index(day_input)
+        end_day_index = (day_index + days_later) % 7
+        end_day = weekdays[end_day_index]
+        day_string = f", {end_day}{day_string}"
+    else:
+        day_string = day_string
+    new_time = f"{final_hour}:{formatted_min} {period}{day_string}"
+
+
+    return new_time
+print(add_time('10:10 PM', '3:30'))
+
+
